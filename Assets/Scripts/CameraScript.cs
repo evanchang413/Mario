2a1fb2221640da22fe56a04085b4813d932daaa8
Ethan Thomas <ethanthomas33@gmail.com>
2015-05-27 13:43:16 -0400

    Jump Fixes, etc

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	Transform player;
+	float offsetX, offsetY;
+
+	// Use this for initialization
+	void Start () {
+		GameObject player_go = GameObject.FindGameObjectWithTag ("Player");
+
+		if (player_go == null) {
+				
+			Debug.LogError("No player object found");
+
+			return;
+		}
+
+		player = player_go.transform;
+			offsetX = transform.position.x - player.position.x;
+//		offsetY = transform.position.y - player.position.y;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (player != null) {
+
+			Vector3 pos = transform.position;
+			pos.x = player.position.x + offsetX;
+			//				pos.y = player.position.y + offsetY;
+
+				transform.position = pos;
+
+		}
+		}
+
+
+
+
+}
--- Assets/Scripts/KoopaControl.cs
+++ Assets/Scripts/KoopaControl.cs
@@ -10,29 +10,66 @@ public class KoopaControl : MonoBehaviour {
 	public Transform groundCheck;
 	float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
-	bool jump;
+
+	Vector3 velocity = Vector3.zero;
 	
+	Animator animator;
+	bool right, left, down, jump, superJump;
+	
+	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 	}
+	//Graphics update here
+	void Update(){
+		if (Input.GetMouseButtonDown (0) || Input.GetKeyDown (KeyCode.Space)) {
+			//			if (transform.position.y >= -0.58f && transform.position.y <= -0.55f)
+			if (GetComponent<Rigidbody2D>().velocity.y < 0.002f && GetComponent<Rigidbody2D>().velocity.y > -0.002f)
+				jump = true;
+		} if (Input.GetKey(KeyCode.RightArrow)) {
+			right = true;
+		} if (Input.GetKey(KeyCode.LeftArrow)) {
+			if (transform.position.x >= 0.6854517f) 
+				left = true;
+		} if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			//			if (transform.position.y >= -0.58f && transform.position.y <= -0.55f)
+			if (GetComponent<Rigidbody2D>().velocity.y < 0.002f && GetComponent<Rigidbody2D>().velocity.y > -0.002f)
+				jump = true;
+		} if (Input.GetKey(KeyCode.DownArrow)) {
+			down = true;
+		} 
+	}
 	
-	
-	void FixedUpdate () {		
-		
+	// Update is called once per frame
+	// Physics updates here
+	void FixedUpdate () {
+
 		float move = Input.GetAxis ("Horizontal");
 		anim.SetFloat("Speed", Mathf.Abs (move));
-		GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);		
-		if (move > 0 && !facingRight)
-			Flip ();
-		else if (move < 0 && facingRight)
-			Flip ();
 
-		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
-			if (GetComponent<Rigidbody2D>().velocity.y < 0.002f || GetComponent<Rigidbody2D>().velocity.y > 0.002f)
-			GetComponent<Rigidbody2D>().AddForce(Vector2.up * 220);
+		if (jump) {
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * 480.0f);
+			//			rigidbody2D.AddForce(Vector2.right * 450.0f);
+			jump = false;
+		} if (right) {
+			transform.position = new Vector2(transform.position.x + 0.025f, transform.position.y);
+			if (!facingRight)
+				Flip ();
+			right = false;
+		} if (left) {
+			transform.position = new Vector2(transform.position.x - 0.025f, transform.position.y);
+			if (facingRight)
+				Flip ();
+			left = false;
+		} if (down) {
+			GetComponent<Rigidbody2D>().AddForce (Vector2.up * -20.0f);
+			down = false;
+		} if (superJump) {
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * 100.0f);
+			superJump = false; 
+		}
 	}
-
-
+	
 	void Flip(){
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
